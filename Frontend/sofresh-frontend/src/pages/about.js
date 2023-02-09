cf93f1bae47162a47541c7cffa7f3ec5cae9c494
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2023-02-09 19:58:31 +0800

    completed about page

--- Frontend/sofresh-frontend/src/pages/about.js
+++ Frontend/sofresh-frontend/src/pages/about.js
@@ -8,11 +8,18 @@ function about() {
       <div>
         <Header />
       </div>
+      <div className='img-container'>
+        <img src={require('../assets/background_img.jpg')} className="bg-img" alt="image not found"/>
+        <p className='overlay-txt'> So Near, It's SoFresh </p>
+      </div>
       <div className='background'>
-        <h1>Hi</h1>
+        <img src={require('../assets/logo-no-background.png')} className='logo' />
+        <h1>About Us</h1>
+        <p className='centered'>SoFresh is an initiative that aims to encourage Singaporeans to purchase locally grown food over imports. By encouraging the purchase of locally grown food, we hope that the increased in demand will help Singapore meet the 30 by 30 goal earlier.</p>
+        <p className='centered'>Join us now in searching for the nearest local produce!</p>
       </div>
     </div>
-    )
+  )
 }
 
 export default about
