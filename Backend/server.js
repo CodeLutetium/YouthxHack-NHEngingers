f7481cbcf894a94076a907544c3ec9a20da33418
lowzk <lowzhekai69@gmail.com>
2023-02-09 15:28:58 +0800

    feat/express_boilerplate

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// app.use('/api', userRoutes);
+
+// app.use('/', (req, res, next) => {
+//     res.status(404).json({
+//         errorMessage: 'No endpoint found.'
+//     })
+// });
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}.`))
