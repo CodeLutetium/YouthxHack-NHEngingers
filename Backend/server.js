341cb3e10adeadd88849d3a728bad724403f28f6
lowzk <lowzhekai69@gmail.com>
2023-02-09 16:41:07 +0800

    feat/databaseconnection

--- Backend/server.js
+++ Backend/server.js
@@ -4,16 +4,18 @@ const port = 3000;
 
 app.use(express.json());
 
-// app.use('/api', userRoutes);
-
-// app.use('/', (req, res, next) => {
-//     res.status(404).json({
-//         errorMessage: 'No endpoint found.'
-//     })
-// });
-
 app.get('/', (req, res) => {
     res.send("Hello world")
 });
+
+// app.use('/api', userRoutes);
+
+app.use('/', (req, res, next) => {
+    res.status(404).json({
+        errorMessage: 'No endpoint found.'
+    })
+});
+
+
 
 app.listen(port, () => console.log(`Server is listening on port ${port}.`))
