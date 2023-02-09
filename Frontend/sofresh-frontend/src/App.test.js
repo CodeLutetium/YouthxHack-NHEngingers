75ab801d0d6629727be129622b1f143f8ba314fc
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2023-02-09 13:35:17 +0800

    searchbar component

--- Frontend/sofresh-frontend/src/App.test.js
+++ Frontend/sofresh-frontend/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import SearchBar from './components/searchBar';
 
 test('renders learn react link', () => {
   render(<App />);
