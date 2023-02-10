37e89e319849f06680ca2c10bf8d89d17ac82b99
Wayne Tan <wtjh00@gmail.com>
2023-02-10 10:41:36 +0800

    feat: add farm_name api call and render on listings

--- Frontend/sofresh-frontend/src/pages/shop.js
+++ Frontend/sofresh-frontend/src/pages/shop.js
@@ -8,13 +8,28 @@ import { useState, useEffect } from 'react'
 function Shop() {
     const [products, setProducts] = useState([]);
 
+    async function getFarmName(farm, farm_id) {
+        const response = await fetch(`http://localhost:3001/api/farm/${farm_id}`, {
+            method: 'GET',
+        })
+        const farmData = await response.json()
+        console.log(farmData)
+        let farmName = farmData[0].farm_name
+        console.log(farmName)
+        farm.farm_name = farmName
+    }
+
     async function getAllProducts() {
         const response = await fetch('http://localhost:3001/api/product', {
             method: 'GET',
         })
         const responseData = await response.json()
-        console.log(responseData)
+        // console.log(responseData)
         setProducts(responseData)
+
+        for (let prod of responseData) {   
+            getFarmName(prod, prod.farm_id)
+        }
     }
     
     useEffect(() =>{
