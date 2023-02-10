3b33b62de7f17a5a7a95366029b5b55f51cf66f8
CodeLutetium <61930995+CodeLutetium@users.noreply.github.com>
2023-02-10 11:25:46 +0800

    sticky header

--- Frontend/sofresh-frontend/src/pages/about.js
+++ Frontend/sofresh-frontend/src/pages/about.js
@@ -5,14 +5,12 @@ import SearchBar from '../components/SearchBar';
 function about() {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
+      <Header />
       <div className='img-container'>
         <img src={require('../assets/background_img.jpg')} className="bg-img" alt="image not found"/>
         <p className='overlay-txt'> So Near, It's SoFresh </p>
       </div>
-      <div className='background'>
+      <div className='background about'>
         <img src={require('../assets/logo-no-background.png')} className='logo' />
         <h1>About Us</h1>
         <p className='centered'>SoFresh is an initiative that aims to encourage Singaporeans to purchase locally grown food over imports. By encouraging the purchase of locally grown food, we hope that the increased in demand will help Singapore meet the 30 by 30 goal earlier.</p>
